2419201d208de69748b222eaf58bbad0fd577226
wxy20030103@icloud.com <3176897365@qq.com>
2025-06-19 14:56:56 +0800

    更改用户注册提示

--- user/form.py
+++ user/form.py
@@ -8,9 +8,9 @@ class MyUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(MyUserCreationForm, self).__init__(*args, **kwargs)
         self.fields['password1'].widget = forms.PasswordInput(
-            attrs={'class': 'txt tabInput', 'placeholder': '密码，4-16位数字/字母/特殊符号（空格除外）'})
+            attrs={'class': 'txt tabInput', 'placeholder': '请输入包含4-16位数字/字母/特殊符号（空格除外）的密码'})
         self.fields['password2'].widget = forms.PasswordInput(
-            attrs={'class': 'txt tabInput', 'placeholder': '重复密码'})
+            attrs={'class': 'txt tabInput', 'placeholder': '请重复输入密码'})
 
     class Meta(UserCreationForm.Meta):
         model = MyUser
@@ -18,8 +18,8 @@ class MyUserCreationForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ('mobile',)
         # 设置模型字段的样式和属性
         widgets = {
-            'mobile': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder': '手机号'}),
-            'username': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder': '用户名'}),
+            'mobile': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder': '请输入手机号'}),
+            'username': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder': '请输入用户名'}),
         }
 
 
