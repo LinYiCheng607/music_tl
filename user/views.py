617023cddbf66a5e99c364b209d5fa31610a178c
wxy20030103@icloud.com <3176897365@qq.com>
2025-06-24 10:40:16 +0800

    更改用户中心显示2

--- user/views.py
+++ user/views.py
@@ -59,7 +59,7 @@ def homeview(request, page):
     # 分页功能
     song_info = request.session.get('play_list', [])
     print(song_info)
-    paginator = Paginator(song_info, 3)
+    paginator = Paginator(song_info, 8)
     try:
         contacts = paginator.page(page)
     except PageNotAnInteger:
