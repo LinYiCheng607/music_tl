a43aea458a85f71d9dfd51a2a4eb27d078705df7
wxy20030103@icloud.com <3176897365@qq.com>
2025-07-01 14:41:25 +0800

    修复合并知识图谱后数据显示问题

--- knowledge_graph/urls.py
+++ knowledge_graph/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.knowledge_graph2, name='knowledge_graph2'),  # 主页面 knowledge_graph.html
-    path('search_kg/', views.knowledge_graph, name='search_kg'),  # 搜索页面 search_knowledge_graph/graph.html
-    # 其它API接口根据需要添加
+    path('', views.knowledge_graph2, name='knowledge_graph2'),  # 主页面
+    path('search_kg/', views.knowledge_graph, name='search_kg'),  # 搜索页面
+    path('search_kg/data/', views.get_graph_data, name='get_graph_data'),  # 新增，用于数据接口
     path('api/graph/', views.graph_data, name='graph_data'),
     path('api/graph_options/', views.graph_options, name='graph_options'),
     path('api/qa/', views.knowledge_graph_qa, name='knowledge_graph_qa'),
